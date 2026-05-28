342932090d1480eb4154700673691be45c483629
Clémence Dolidon <clemdolidon@gmail.com>
2026-05-28 15:19:34 +0200

    systeme mais ya rien

--- Systeme/CalculateurView.cs
+++ Systeme/CalculateurView.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Systeme
 {
-    internal class CalculateurView
+    public class CalculateurView 
     {
+        
     }
 }
